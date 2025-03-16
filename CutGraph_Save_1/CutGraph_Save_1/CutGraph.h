c645b8ad67722e93b1a3760a079f5094401259bc
mortovid <shenlinhan@gmail.com>
2025-03-16 14:38:07 -0400

    test

---
+++ CutGraph_Save_1/CutGraph_Save_1/CutGraph.h
@@ -0,0 +1,64 @@
+#ifndef _CUT_GRAPH_H_
+#define _CUT_GRAPH_H_
+
+#include "CutGraphMesh.h"
+
+namespace MeshLib
+{
+    /*! \brief CCutGraph class
+     *
+     *   Compute the spanning tree of the dual mesh,
+     *   the edges whose duals are not on the tree form the cut locus,
+     *   label the cut locus as the sharp edges.
+     */
+
+
+    void embed(CCutGraphVertex v0, CCutGraphVertex v1, CCutGraphVertex v2, CPoint* proj0, CPoint* proj1, CPoint* proj2);
+
+    class CCutGraph
+    {
+    public:
+        /*!
+         *  CCutGraph constructor
+         *  \param pMesh input closed mesh
+         */
+        CCutGraph(CCutGraphMesh* pMesh) { m_pMesh = pMesh; };
+
+        /*!
+         * Compute the cut graph.
+         */
+        void cut_graph();
+
+        /*! Computes the curvatures and valences for each vertex [UNTESTED] */
+        void compCurvature();
+
+        /*! Computes the dihedral angle for each edge */
+        void compDihedralVertAngles();
+
+        /*! Computes the power of each halfedge */
+        void compEdgePower();
+
+        /*! Computes the Total Scalar Curvature */
+        float compTSC();
+
+        /*! Performs the initial the length, boundary, valence, and height calculations */
+        void initGraph();
+
+    protected:
+        /*!
+         *  Input closed mesh.
+         */
+        CCutGraphMesh* m_pMesh;
+
+        /*!
+         *  Compute the spanning tree of the dual mesh.
+         */
+        void _dual_spanning_tree();
+
+        /*!
+         * Prune the branches which attached to valence-1 nodes.
+         */
+        void _prune();
+    };
+} // namespace MeshLib
+#endif // !_CUT_GRAPH_H_
